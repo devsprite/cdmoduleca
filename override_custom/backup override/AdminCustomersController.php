314e5d3e5b1123c07939e2f47227784286b34aa7
Dominique <dominique@chez-dominique.fr>
2016-11-07 17:11:01 +0100

    fix standarts

--- override_custom/backup override/AdminCustomersController.php
+++ override_custom/backup override/AdminCustomersController.php
@@ -1,13 +1,15 @@
 <?php
+
 /*
-* 2007-2015 PrestaShop
+* 2007-2016 PrestaShop
 *
 * NOTICE OF LICENSE
 *
-* This source file is subject to the Open Software License (OSL 3.0)
+* This source file is subject to the Academic Free License (AFL 3.0)
 * that is bundled with this package in the file LICENSE.txt.
 * It is also available through the world-wide-web at this URL:
-* http:* If you did not receive a copy of the license and are unable to
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
 * obtain it through the world-wide-web, please send an email
 * to license@prestashop.com so we can send you a copy immediately.
 *
@@ -15,11 +17,14 @@
 *
 * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
 * versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http:*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2015 PrestaShop SA
-*  @license    http:*  International Registered Trademark & Property of PrestaShop SA
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author Dominique <dominique@chez-dominique.fr>
+*  @copyright  2007-2016 Chez-dominique
 */
+
+if (!defined('_PS_VERSION_'))
+    exit;
 
 class AdminCustomersController extends AdminCustomersControllerCore
 {
