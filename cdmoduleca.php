4a5590bb91ad23935ece823c3d9c078c0ec2df28
Devsprite <dominique@chez-dominique.fr>
2017-03-30 19:27:55 +0200

    version

--- cdmoduleca.php
+++ cdmoduleca.php
@@ -64,7 +64,7 @@ class CdModuleCA extends ModuleGrid
     {
         $this->name = 'cdmoduleca';
         $this->tab = 'analytics_stats';
-        $this->version = '1.0.21';
+        $this->version = '1.0.22';
         $this->author = 'Dominique';
         $this->need_instance = 0;
         $this->bootstrap = true;
